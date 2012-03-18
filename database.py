327582d9ace3efd69fbdb68af056cef465786999
fesh0r <dev@fesh.geek.nz>
2012-03-19 11:48:05 +1300

    kill DBLock, and use sqlite3 normal locking/transactions

--- database.py
+++ database.py
@@ -6,21 +6,12 @@ from contextlib import closing
 #====================== DB Decorator ===================================
 def database(f):
     def wrap_f(*args, **kwargs):
-        if not 'DBLock' in globals():
-            globals()['DBLock'] = threading.Lock()
+        with sqlite3.connect('database.sqlite', timeout=30, isolation_level='DEFERRED') as con:
+            con.text_factory = sqlite3.OptimizedUnicode
+            with closing(con.cursor()) as cur:
+                (idversion,) = cur.execute("""SELECT value FROM config WHERE name='currentversion'""").fetchone()
 
-        try:
-            DBLock.acquire()
-            with sqlite3.connect('database.sqlite') as con:
-                con.text_factory = sqlite3.OptimizedUnicode
-                with closing(con.cursor()) as cur:
-                    (idversion,) = cur.execute("""SELECT value FROM config WHERE name='currentversion'""").fetchone()
-
-                    kwargs['cursor'] = cur
-                    kwargs['idvers'] = idversion
-                    f(*args, **kwargs)
-            DBLock.release()
-        except Exception:
-            DBLock.release()
-            raise
+                kwargs['cursor'] = cur
+                kwargs['idvers'] = idversion
+                f(*args, **kwargs)
     return wrap_f
